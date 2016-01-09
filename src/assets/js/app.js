29095da9fa8b6d2ea58420d1553a70b3e2577bae
Joe Paolicelli <joe@joepaolicelli.com>
2016-01-09 16:50:26 -0500

    Changes to site header. New font (Glacial Indifference Bold) for the site title, font files included in this commit.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,48 @@
 $(document).foundation();
+
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
+
+// Make site title stay on one line and be close to the div bottom border.
+
+$(".site-title > h1").fitText(0.8);
+
+var myHeaderHeight = $(".my-header").height();
+var siteTitleHeight = $(".site-title > h1").height();
+
+var adjustHeaderPadding = function () {
+    $(".site-title").css("padding-top", myHeaderHeight - siteTitleHeight);
+}
+
+adjustHeaderPadding();
