70236cd5897eb1db0feedcf60267c35be0248742
Joe Paolicelli <joe@joepaolicelli.com>
2016-01-23 17:05:00 -0500

    Made many changes to page header, added sticky footer. Added homepage and homepage styles.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,48 +1,30 @@
 $(document).foundation();
 
+if(Foundation.MediaQuery.atLeast('medium')) {
 
-(function( $ ){
+  var myHeaderHeight = $(".my-header").height();
+  var siteTitleHeight = $(".site-title > h1").height();
+  var headerLinksHeight = $(".header-links").height();
 
-  $.fn.fitText = function( kompressor, options ) {
+  var adjustHeaderPadding = function () {
+      $(".site-title").css("padding-top", myHeaderHeight - siteTitleHeight);
+      $(".header-links").css("padding-top", myHeaderHeight - headerLinksHeight);
+  }
 
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
+  adjustHeaderPadding();
 
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
-
-// Make site title stay on one line and be close to the div bottom border.
-
-$(".site-title > h1").fitText(0.8);
-
-var myHeaderHeight = $(".my-header").height();
-var siteTitleHeight = $(".site-title > h1").height();
-
-var adjustHeaderPadding = function () {
-    $(".site-title").css("padding-top", myHeaderHeight - siteTitleHeight);
+  $(window).on('resize', adjustHeaderPadding);
 }
 
-adjustHeaderPadding();
+/* Sticky Footer */
+/* http://codepen.io/imohkay/pen/htpzf */
+$(document).ready(function(){
+  $(window).resize(function(){
+      var footerHeight = $('#footer').outerHeight();
+      var stickFooterPush = $('#push').height(footerHeight);
+
+      $('#wrapper').css({'marginBottom':'-' + footerHeight + 'px'});
+    });
+
+    $(window).resize();
+  });
